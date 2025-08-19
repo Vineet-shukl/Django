8d9e3224aa4b02876ff1e1ca05417b2282a289dd
vineetshukla.work@gmail.com <vineetshukla.work@gmail.com>
2025-08-19 14:59:29 +0530

    time:2:59

--- myfirst/blog/admin.py
+++ myfirst/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.models import Users, Posts
+
+
+admin.site.register(Users)  # Assuming you have a model to register
 
 # Register your models here.
+admin.site.register(Posts)
--- myfirst/blog/models.py
+++ myfirst/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    dob=models.DateField()
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=5000)
+    pub_date = models.DateTimeField('date published')
+    modified_date = models.DateTimeField('date modified')
+    user = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
+
+
+
+
